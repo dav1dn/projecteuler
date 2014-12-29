c83e1d8a28c8810b3bbf95d814b58b2fdd9b3297
emesde <nougatcrunch@gmail.com>
2014-12-28 21:37:30 -0800

    four

---
+++ python/004.py
@@ -0,0 +1,35 @@
+"""A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers."""
+
+import time
+
+def is_palindrome(n):
+    # Sorta hacky way to determine if a number is a palindrome.
+    return str(n) == str(n)[::-1]
+
+def three_digit_multiples_of_11():
+    lst = [11*n for n in range(1,100)]
+    return [n for n in lst if len(str(n)) == 3]
+
+def solution():
+    # Each even-digited palindrome is divisible by 11, so we'll use multiples of 11.
+    # Proof: http://jwilson.coe.uga.edu/emt669/Student.Folders/Bacchus.Mohamed/pal/pal.html
+    # We'll bruteforce each 3 digit multiple of 11 with each 3 digit number. 
+    # This is pretty inefficient...
+    largest_palindrome = 1
+
+    for x in three_digit_multiples_of_11():
+        for y in list(range(100,999)):
+            product = x * y
+            if is_palindrome(product) and product > largest_palindrome:
+                largest_palindrome = product
+
+    return largest_palindrome
+    
+
+    
+if __name__ == '__main__':
+    start = time.time()
+    answer = solution()
+    print("answer {0} found in {1} seconds".format(answer, time.time() - start))
